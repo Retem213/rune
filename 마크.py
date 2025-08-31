1dc7bf20603b71800f389a527f9bb44a85a886ef
Retem213 <125482795+Retem213@users.noreply.github.com>
2025-08-31 23:57:01 +0900

    Update 마크.py

--- 마크.py
+++ 마크.py
@@ -91,7 +91,8 @@ data = {
     ],
     "Wasobeso": [
         {"name": "부성1", "location": [-2232, 67, 44]},
-        {"name": "임시", "location": [219, -331, 80]},
+        {"name": "임시 글리아 1", "location": [219, 80, -331]},
+        {"name": "임시 글리아 2", "location": [1026, 70, 53]},
     ],
     "Tangled Dahye": [
         {"name": "임시", "location": [-2491, 60, 271]},
@@ -408,6 +409,7 @@ elif tab_option == "가상 지도":
 elif tab_option == "전쟁지도":
     st.title("전쟁지도")
     plot_virtual_map_interactive(data, mode="war")
+
 
 
 
