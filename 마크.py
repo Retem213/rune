c4a825427a707b49a00631d5f343e3a056e1c9bb
Retem213 <125482795+Retem213@users.noreply.github.com>
2025-10-19 23:00:02 +0900

    Update 마크.py

--- 마크.py
+++ 마크.py
@@ -101,6 +101,7 @@ data = {
     "Tangled Dahye": [ 
         {"name": "부성 파괴", "location": [-2232, 67, 44]},
         {"name": "부성 파괴", "location": [1026, 70, 53]},
+        {"name": "누군가의 성", "location": [440, 67, -1640]},
     ]
 }
 
@@ -414,6 +415,7 @@ elif tab_option == "가상 지도":
 elif tab_option == "전쟁지도":
     st.title("전쟁지도")
     plot_virtual_map_interactive(data, mode="war")
+
 
 
 
